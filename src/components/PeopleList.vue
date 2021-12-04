67e58f86b43fc3b7569182c011eb80b6264a4b6b
Damien McDonnell <dm@cct-marketing.com>
2021-12-04 20:00:12 +0000

    Initial list of people working and basic styling

---
+++ src/components/PeopleList.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="people-list-wrapper flex align-middle justify-center">
+    <table class="people-list w-full">
+      <tr class="people-list__header bg-gray-100">
+        <th class="people-list__header__item">Name</th>
+        <th class="people-list__header__item">Height</th>
+        <th class="people-list__header__item">Mass</th>
+        <th class="people-list__header__item">Created</th>
+        <th class="people-list__header__item">Edited</th>
+        <th class="people-list__header__item">Planet</th>
+      </tr>
+      <tr
+        v-for="person in people"
+        :key="person.url"
+        class="people-list__person"
+      >
+        <td class="people-list__person__item people-list__person__name">
+          {{ person.name }}
+        </td>
+        <td class="people-list__person__item people-list__person__height">
+          {{ person.height }}
+        </td>
+        <td class="people-list__person__item people-list__person__mass">
+          {{ person.mass }}
+        </td>
+        <td class="people-list__person__item people-list__person__created">
+          {{ person.created }}
+        </td>
+        <td class="people-list__person__item people-list__person__edited">
+          {{ person.edited }}
+        </td>
+        <td class="people-list__person__item people-list__person__planet">
+          {{ person.homeworld }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'PeopleList',
+  props: {
+    msg: String,
+  },
+  mounted() {
+    this.$store.dispatch('GET_PEOPLE');
+  },
+  computed: mapState([
+    'people',
+  ]),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.people-list {
+  background-color: $table-body;
+
+  &__header {
+    background-color: $table-header;
+    color: white;
+
+    &__item {
+      padding: 0.5rem 1rem;
+    }
+  }
+
+  &__person {
+    border-top: solid 1px $table-border;
+    &:last-child {
+      border-bottom: solid 1px $table-border;
+    }
+
+    &__item {
+      padding: 0.5rem 1rem;
+    }
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue';
+import axios from 'axios';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    currentPeoplePage: 1,
+    people: {},
+    planets: {},
+  },
+  getters: {
+    people: (state) => state.people,
   },
   mutations: {
+    SET_PEOPLE(state, people) {
+      state.people = people;
+    },
   },
   actions: {
+    GET_PEOPLE({ commit }) {
+      axios
+        .get('https://swapi.dev/api/people/')
+        .then((response) => response.data)
+        .then((data) => {
+          console.log(data);
+          commit('SET_PEOPLE', data.results);
+        });
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
